da8e6d32dc905a9ff181a8411c975b19880c4dd8
FirosStuart <firosstuart@gmail.com>
2022-03-21 00:13:21 +0900

    Update containerd.sh && Rename ufw to network.sh

--- containerd.sh
+++ containerd.sh
@@ -5,4 +5,5 @@ wget https://github.com/containerd/containerd/releases/download/v1.6.1/cri-conta
 sudo tar --no-overwrite-dir -C / -xzf cri-containerd-cni-1.6.1-linux-amd64.tar.gz
 sudo systemctl daemon-reload
 sudo systemctl enable --now containerd
-containerd config default > /etc/containerd/config.toml
+sudo mkdir -p /etc/containerd
+containerd config default > config.toml && sudo mv config.toml /etc/containerd/config.toml
