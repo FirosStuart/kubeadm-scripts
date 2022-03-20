f006178518d0474d0333fb469f632184b7ed1df0
FirosStuart <firosstuart@gmail.com>
2022-03-21 00:34:40 +0900

    Add kubernetes cluster init

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eu
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
